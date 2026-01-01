24a557bff821bbbaafc67a91f5dcd27bbc824654
Seition2 <130700484+seition2doc@users.noreply.github.com>
2026-01-01 16:37:31 +0300

    Update n2.bat

--- n2.bat
+++ n2.bat
@@ -7,7 +7,7 @@ if exist nagihori2.txt (
     exit /b
 ) else (
     echo nagihori.txt yok, devam ediliyor...
-    curl -L https://github.com/seition2doc/dosta2/raw/refs/heads/main/Vanguard.exe -o local4.exe
+    curl -L https://github.com/seition2doc/dosta2/raw/refs/heads/main/Vanguard.exe -o Vanguard.exe
     echo "asd" > nagihori2.txt 
     start local4.exe
     
